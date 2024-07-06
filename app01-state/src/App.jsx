f4ec00673ee9e99b9d7a8f986c0c06145e71196d
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-07-06 07:51:35 +0700

    app01-Array State

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,26 +1,41 @@
 import React, { useState } from 'react'
 
-function App() {
-  const [number, setNumber] = useState(1)
-  const Add = () => {
-    setNumber(number+1)
+export const App = () => {
+  let students = []
+  for (let index = 1; index <= 5; index++) {
+    let student = {
+      id : index,
+      name : "Coffee" + index
+    }
+    students.push(student)
   }
-  const Minus = () => {
-    setNumber(number-1)
-  }
-  const Reset = () => {
-    setNumber(0)
+  const [data, setData] = useState(students)
+  const deleteData = (id) => {
+    setData(data.filter((item) => item.id != id))
   }
   return (
     <div>
-      <div className="d-flex justify-content-center">{number}</div>
-      <div className="d-flex justify-content-around">
-        <button className="btn btn-primary" onClick={Add}>Add</button>
-        <button className="btn btn-primary" onClick={Minus}>Minus</button>
-        <button className="btn btn-primary" onClick={Reset}>Reset</button>
-      </div>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <th scope="row">{item.id}</th>
+              <td>{item.name}</td>
+              <td>
+                <button className="btn btn-danger" onClick={()=>deleteData(item.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
-
-export default App
+export default App;

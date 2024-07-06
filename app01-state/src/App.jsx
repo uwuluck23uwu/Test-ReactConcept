3d345ec1a0d7bc6e99a80391aeef7c113da6fec1
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-07-06 15:13:13 +0700

    app01-props from parent to child

--- app01-state/src/App.jsx
+++ app01-state/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Header from './components/Header'
 import StudentList from './components/StudentList'
 
 function App() {
+  let students = []
+  for (let index = 1; index <= 5; index++) {
+      let student = {
+        id : index,
+        name : "Coffee" + index
+      }
+      students.push(student)
+  }
+  const [data, setData] = useState(students)
+  const deleteData = (id) => setData(data.filter((item) => item.id != id))
   return (
     <div>
       <Header title="Student list" major="Comcience"/>
-      <StudentList/>
+      <StudentList data={data} deleteData={deleteData}/>
     </div>
   )
 }
---
+++ app01-state/src/components/Student.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Student(data, deleteData) {
+  return (
+    <div>
+        <table className="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Action</th>
+            </tr>
+            </thead>
+            <tbody>
+            {data.map((item) => (
+                <tr key={item.id}>
+                <th scope="row">{item.id}</th>
+                <td>{item.name}</td>
+                <td>
+                    <button className="btn btn-danger" onClick={()=>deleteData(item.id)}>Delete</button>
+                </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Student

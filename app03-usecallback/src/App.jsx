c89512e0c4270a1246c7fd9d6ec61aec811d4426
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-07-07 14:09:35 +0700

    app03-no useCallback

---
+++ app03-usecallback/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const Child = ({ returnComment }) => {
+  useEffect(() => {
+    console.log("CHILD WAS CALLED");
+  }, [returnComment]);
+
+  return <h4>Show message : {returnComment("2.Child")}</h4>;
+};
+
+const App = () => {
+  const [toggle, setToggle] = useState(false);
+  const [data, setData] = useState("1.App - ");
+
+  const returnComment = (name) => {
+    return data + name;
+  };
+
+  return (
+    <div className="App">
+      <Child returnComment={returnComment} />
+      <button className="btn btn-primary" onClick={() => { setToggle(!toggle); setData("ทดสอบเปลี่ยนค่า - "); }}>Click Toggle</button>
+      {toggle && <h1> toggle </h1>}
+    </div>
+  );
+};
+export default App;

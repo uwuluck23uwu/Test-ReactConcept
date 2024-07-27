e40be0b0e6af6ab5a0a572f1533b11f0e5be828f
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-07-27 09:21:15 +0700

    app06 - Cart Component

--- app06-context-reducer/src/context/CartContext.js
+++ app06-context-reducer/src/context/CartContext.js
@@ -1,5 +1,6 @@
 import { createContext,useContext} from "react"
 import { products } from "../data/product"
+import { cartReducer } from "../reducer/cartReducer";
 
 const CartContext = createContext();
 
@@ -9,6 +10,19 @@ const initState = {
     amount:0
 }
 
+export const CartProvider = ({ children }) => {
+    //ส่งค่าเข้าไปทำงานสองค่าคือ cartReducer และ initState
+    //และ return ค่าที่จะนำไปใช้ต่อสองค่าคือ state และ dispatch
+    const [state, dispatch] = useReducer(cartReducer, initState);
+  
+    //กระจายข้อมูลที่จะนำไปใช้งานหรือแชร์ด้วย Context Provider ผ่านคุณสมบัติ value
+    return (
+      <CartContext.Provider value={{ ...state }}>
+        {children} {/* คอมโพเนนต์ที่จะทำ value ไปใช้งาน */}
+      </CartContext.Provider>
+    );
+  };
+  
 export const useCart = ()=>{
     return useContext(CartContext);
 }

d1beb363036502d74e9dfa9d58e51d1e56a1b2da
Sumo <144433156+uwuluck23uwu@users.noreply.github.com>
2024-07-27 09:15:00 +0700

    app06 - Create Provider in Context

--- app06-context-reducer/src/context/CartContext.js
+++ app06-context-reducer/src/context/CartContext.js
@@ -1,6 +1,13 @@
 import { createContext,useContext} from "react"
+import { products } from "../data/product"
 
 const CartContext = createContext();
+
+const initState = {
+    products:products,
+    total:0,
+    amount:0
+}
 
 export const useCart = ()=>{
     return useContext(CartContext);
